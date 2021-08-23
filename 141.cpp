726a548f29629b3e27da3c0184e106136572fa06
thechildwholovesyou <ssecutities@gmail.com>
2021-08-23 10:32:56 +0530

    4

---
+++ 141.cpp
@@ -0,0 +1,21 @@
+// 141. Linked List Cycle
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head==NULL or head->next==NULL) return false;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        
+        while(slow!=NULL and fast!=NULL and fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            
+            if(slow==fast) return true;
+        }
+        return false;
+        
+       
+    }
+};
