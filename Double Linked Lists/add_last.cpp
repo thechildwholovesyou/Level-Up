2c806507348dd61677f33778d00ada5aac63ec67
thechildwholovesyou <ssecutities@gmail.com>
2021-08-23 12:55:43 +0530

    doublyLL2

---
+++ Double Linked Lists/add_last.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class DoublyLinkedList
+{
+  class Node
+  {
+  public:
+    int data = 0;
+    Node* prev = nullptr;
+    Node* next = nullptr;
+
+    Node(int data)
+    {
+      this->data = data;
+    }
+  };
+
+  Node* head = nullptr;
+  Node* tail = nullptr;
+  int size = 0;
+
+public:
+  void display()
+  {
+    string str = "";
+    Node* curr = this->head;
+    str += "[";
+    while (curr != nullptr)
+    {
+      str += to_string(curr->data);
+      if (curr->next != nullptr)
+        str += ", ";
+      curr = curr->next;
+    }
+    str += "]";
+
+    cout << str << endl;
+  }
+
+private:
+  void addFirstNode(Node* node)
+  {
+    if (this->size == 0)
+      this->head = this->tail = node;
+    else
+    {
+      node->next = this->head;
+      this->head->prev = node;
+      this->head = node;
+    }
+    this->size++;
+  }
+
+public:
+  void addFirst(int data)
+  {
+    Node* node = new Node(data);
+    addFirstNode(node);
+  }
+
+  void addLast(int data)
+  {
+    // complete this function.
+    if(size==0)
+    {
+        addFirst(data);
+    }
+    else
+    {
+        Node* node=new Node(data);
+        tail->next=node;
+        node->prev=tail;
+        tail=node;
+    }
+    size++;
+  }
+};
+
+int main()
+{
+  DoublyLinkedList dll;
+  string s;
+  while (getline(cin, s))
+  {
+    stringstream ss(s);
+    string word;
+    ss >> word;
+    if (word == "stop")
+      break;
+    if (word == "addFirst")
+    {
+      int data;
+      ss >> data;
+      dll.addFirst(data);
+    }
+    else if (word == "addLast")
+    {
+      int data;
+      ss >> data;
+      dll.addLast(data);
+    }
+  }
+
+  dll.display();
+
+  return 0;
+}
