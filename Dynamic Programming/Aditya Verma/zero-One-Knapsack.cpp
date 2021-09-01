a5b78f5a271b9832ce3f794491045c76dba8572e
thechildwholovesyou <ssecutities@gmail.com>
2021-09-01 14:13:42 +0530

    tabu_updated

--- Dynamic Programming/Aditya Verma/zero-One-Knapsack.cpp
+++ Dynamic Programming/Aditya Verma/zero-One-Knapsack.cpp
@@ -67,4 +67,49 @@ class Solution
     }
 };
 
+// tabu code
 
+class Solution
+{
+    public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    
+    int knapSack_tabu(int W, int wt[], int val[], int n,vector<vector<int>>&dp)
+    {
+        // initialization step 
+        for(int i=0;i<dp.size();i++)
+        {
+            for(int j=0;j<dp[0].size();j++)
+            {
+                if(i==0 or j==0)
+                    dp[i][j]=0;
+            }
+        }
+        
+        // choice diagram 
+        for(int i=1;i<dp.size();i++)
+        {
+            for(int j=1;j<dp[0].size();j++)
+            {
+                if(wt[i-1]<=j)
+                {
+                    dp[i][j]=max(val[i-1]+dp[i-1][j-wt[i-1]], dp[i-1][j]);
+                }
+                else if(wt[i-1]>j)
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+                
+            }
+        }
+        return dp[n][W];
+    }
+    
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       // Your code here
+       vector<vector<int>> dp( n+1, vector<int> (W+1, -1));
+       int ans=knapSack_tabu(W,wt,val,n,dp);
+       return ans;
+    }
+};
