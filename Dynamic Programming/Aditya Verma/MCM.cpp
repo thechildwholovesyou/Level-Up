31d5e960572cdb63cf2d51aab7cb15ca746543da
thechildwholovesyou <ssecutities@gmail.com>
2021-09-08 10:31:29 +0530

    MCM_rec

--- Dynamic Programming/Aditya Verma/MCM.cpp
+++ Dynamic Programming/Aditya Verma/MCM.cpp
@@ -1,1 +1,31 @@
 // Matrix Chain Multiplication 
+// https://practice.geeksforgeeks.org/problems/matrix-chain-multiplication0303/1
+// rec code
+
+class Solution{
+public:
+
+    int solve_rec(int arr[],int i,int j)
+    {
+        // base condition 
+        if(i>=j) return 0;
+        
+        int mini = INT_MAX;
+        
+        for(int k=i;k<=j-1;k++)
+        {
+            int tempAns = solve_rec(arr,i,k)+solve_rec(arr,k+1,j) + (arr[i-1]*arr[k]*arr[j]);
+            
+            mini=min(tempAns, mini);
+        }
+        return mini;
+        
+    }
+    int matrixMultiplication(int N, int arr[])
+    {
+        // code here
+        
+        return solve_rec(arr,1,N-1);
+        
+    }
+};
