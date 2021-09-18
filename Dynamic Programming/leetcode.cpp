86b44af0649b0e781d338ed60fc23ca65669f92d
thechildwholovesyou <ssecutities@gmail.com>
2021-09-18 19:54:29 +0530

    416. Partition Equal Subset Sum

--- Dynamic Programming/leetcode.cpp
+++ Dynamic Programming/leetcode.cpp
@@ -1238,3 +1238,137 @@ public:
         return egg(2 , n);
     }
 };
+
+// 416. Partition Equal Subset Sum
+// https://leetcode.com/problems/partition-equal-subset-sum/
+// rec code
+
+class Solution {
+public:
+    
+    bool helper(vector<int>&nums, int target, int n)
+    {
+        if(n==0) return false;
+        if(target==0) return true;
+        
+        bool ans =false;
+        if(nums[n-1]<=target)
+        {
+            bool a1=helper(nums, target-nums[n-1],n-1);
+            bool a2=helper(nums, target,n-1);
+            ans = a1 || a2;
+        }
+        else if(nums[n-1]>target)
+            return helper(nums, target,n-1);
+        
+        return ans;
+            
+    }
+    
+    bool canPartition(vector<int>& nums) {
+        int sum=0;
+        for(auto ele: nums)
+            sum+=ele;
+        
+        if(sum%2!=0) return false;
+        return helper(nums,sum/2,nums.size());
+        
+    }
+};
+
+// memo code
+
+class Solution {
+public:
+    
+    bool subsetSum_mem(vector<int>&nums, int sum,int n,vector<vector<int>>&dp)
+    {
+        if(sum==0) return true;
+        if(n==0) return false;
+        bool ans=false;
+        
+        if(dp[n][sum]!=-1)
+            return dp[n][sum];
+        
+        if(nums[n-1]<=sum)
+        {
+            bool a1=subsetSum_mem(nums,sum-nums[n-1],n-1,dp);
+            bool a2=subsetSum_mem(nums,sum,n-1,dp);
+            
+            ans=a1 or a2;
+            dp[n][sum]=ans;
+        }
+        else if(nums[n-1]>sum)
+            return dp[n][sum]= subsetSum_mem(nums,sum,n-1,dp);
+        return dp[n][sum];
+    }
+    
+    bool findPartition(vector<int>&nums)
+    {
+        int n=nums.size();
+        int sum=0;
+        for(auto ele: nums)
+            sum+=ele;
+        if(sum%2!=0)
+            return false;
+        vector<vector<int>> dp( n+1 , vector<int> (sum/2+1, -1));
+        return subsetSum_mem(nums,sum/2,n,dp);
+    }
+    
+    bool canPartition(vector<int>& nums) {
+        return findPartition(nums);
+    }
+};
+
+// tabu code
+
+class Solution {
+public:
+    
+    bool helper(vector<int>&nums, int target,vector<vector<int>>&dp)
+    {
+
+        for(int i=0;i<dp.size();i++)
+        {
+            for(int j=0;j<dp[0].size();j++)
+            {
+                if(i==0)
+                    dp[i][j]=false;
+                if(j==0)
+                    dp[i][j]=true;
+            }
+        }
+        
+        bool ans =false;
+        // main dp
+        
+        for(int i=1;i<dp.size();i++)
+        {
+            for(int j=1;j<dp[0].size();j++)
+            {
+                if(nums[i-1]<=j)
+                {
+                    bool a1=dp[i-1][j-nums[i-1]];
+                    bool a2=dp[i-1][j];
+                    
+                    dp[i][j]=a1 or a2;
+                }
+                else if(nums[i-1]>j)
+                    dp[i][j]=dp[i-1][j];
+            }
+        }
+        return dp[dp.size()-1][dp[0].size()-1];
+            
+    }
+    
+    bool canPartition(vector<int>& nums) {
+        int sum=0;
+        for(auto ele: nums)
+            sum+=ele;
+        
+        if(sum%2!=0) return false;
+        vector<vector<int>>dp(nums.size()+1, vector<int>(sum/2+1,-1));
+        return helper(nums,sum/2,dp);
+        
+    }
+};
