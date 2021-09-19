29787c6d5b51e8475807162558c9a8d97a202f90
thechildwholovesyou <ssecutities@gmail.com>
2021-09-19 21:12:23 +0530

    312. Burst Balloons

---
+++ Dynamic Programming/cutStrategy.cpp
@@ -0,0 +1,4 @@
+// MATRIX CHAIN MULTIPLICATION 
+
+// https://practice.geeksforgeeks.org/problems/matrix-chain-multiplication0303/1
+
--- Dynamic Programming/leetcode.cpp
+++ Dynamic Programming/leetcode.cpp
@@ -1570,3 +1570,39 @@ class Solution {
         return find(m,n,M,r,c);
     }
 };
+
+
+// 312. Burst Balloons
+// https://leetcode.com/problems/burst-balloons/
+
+class Solution {
+public:
+    
+     int dp[502][502];
+    
+    int solve_memo(vector<int>&arr,int i,int j)
+    {
+        // base condition 
+        if(i>=j) return 0;
+        
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int ans =INT_MIN;
+        for(int k=i;k<j;k++)
+        {
+            int tempAns = solve_memo(arr,i,k)+solve_memo(arr,k+1,j) + (arr[i-1]*arr[k]*arr[j]);
+            
+            ans=max(ans,tempAns);
+        }
+        return dp[i][j]=ans;
+        
+    }
+    
+    int maxCoins(vector<int>& nums) {
+        nums.insert(nums.begin(), 1);
+        nums.insert(nums.end(), 1);
+        memset(dp,-1,sizeof(dp));
+        return solve_memo(nums,1,nums.size()-1);
+    }
+};
