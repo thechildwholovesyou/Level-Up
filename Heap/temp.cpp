43be77dfda1f8685c494031d8c6b441d9149f08d
thechildwholovesyou <ssecutities@gmail.com>
2021-09-20 10:42:52 +0530

    215. Kth Largest Element in an Array

---
+++ Heap/temp.cpp
@@ -0,0 +1,16 @@
+// 215. Kth Largest Element in an Array
+// https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>, greater<int>> pq; // min heap 
+        for(int i=0;i<nums.size();i++)
+        {
+            pq.push(nums[i]);
+            if(pq.size()>k)
+                pq.pop();
+        }
+        return pq.top();
+    }
+};
