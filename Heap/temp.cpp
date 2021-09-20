ba85299ed8d0a2960d5726b9a810745d2c0f6eac
thechildwholovesyou <ssecutities@gmail.com>
2021-09-20 15:08:46 +0530

    658. Find K Closest Elements

--- Heap/temp.cpp
+++ Heap/temp.cpp
@@ -200,3 +200,27 @@ public:
         
     }
 };
+
+
+// 658. Find K Closest Elements
+// https://leetcode.com/problems/find-k-closest-elements/
+
+class Solution {
+public:
+   vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pair<int, int>> pq;
+        vector<int> ans;
+        for(int i = 0; i < arr.size(); i++){
+           
+            pq.push({(abs(arr[i]-x)),arr[i]});
+            if(pq.size() > k)
+                pq.pop();
+        }
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
