66507357f597ce7b827742c88a6f4c00bb04e25c
thechildwholovesyou <ssecutities@gmail.com>
2021-09-20 16:08:33 +0530

    692. Top K Frequent Words

--- Heap/temp.cpp
+++ Heap/temp.cpp
@@ -316,3 +316,41 @@ public:
         return str;
     }
 };
+
+// 692. Top K Frequent Words
+// https://leetcode.com/problems/top-k-frequent-words/
+
+
+class cmp{
+    public:
+    bool operator()(pair<int,string> a, pair<int,string> b) const{
+        if(a.first < b.first) return true;
+        else if(a.first == b.first && a.second>b.second) return true;
+        return false;
+    }
+};
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        map<string,int> m;
+        for(auto ele: words)
+        {
+            m[ele]++;
+        }
+        priority_queue<pair<int,string>,vector<pair<int,string>>,cmp> pq;
+        vector<string> ans;
+        
+        for(auto ele: m)
+        {
+            pq.push({ele.second, ele.first});
+        }
+        
+        while(k--){
+            
+           ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        // reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
