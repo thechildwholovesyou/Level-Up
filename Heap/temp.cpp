37c913cddbfc93aad5249e72479b8b78eb1a5524
thechildwholovesyou <ssecutities@gmail.com>
2021-09-20 15:48:00 +0530

    347. Top K Frequent Elements

--- Heap/temp.cpp
+++ Heap/temp.cpp
@@ -256,3 +256,32 @@ public:
         return 0;
     }
 };
+
+// 347. Top K Frequent Elements
+// https://leetcode.com/problems/top-k-frequent-elements/
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        priority_queue<pair<int,int>> pq;
+        
+        map<int, int> m;
+        for(int i=0;i<nums.size();i++)
+        {
+            m[nums[i]]++;
+        }
+        
+        for(auto ele: m)
+        {
+            pq.push({ele.second, ele.first});
+        }
+        
+        vector<int> ans;
+        while(k--)
+        {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
