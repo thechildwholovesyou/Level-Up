52652c2ef2d037bcd59822ace742bbd849cb41b8
thechildwholovesyou <ssecutities@gmail.com>
2021-09-21 09:57:32 +0530

    968. Binary Tree Cameras

---
+++ Binary Trees/level2.cpp
@@ -0,0 +1,29 @@
+// 968. Binary Tree Cameras
+// https://leetcode.com/problems/binary-tree-cameras/
+
+class Solution {
+public:
+    
+    int cameras;
+    int minCameraCover_helper(TreeNode* root)
+    {
+        if(root==NULL) return 1;
+        
+        int lc=minCameraCover_helper(root->left);
+        int rc=minCameraCover_helper(root->right);
+        
+        if(lc==-1 || rc==-1){
+            cameras++;
+            return 0;
+        }
+        if(lc==0 || rc==0) return 1;
+        
+        return -1;
+    }
+    
+    int minCameraCover(TreeNode* root) {
+        if(minCameraCover_helper(root)==-1) cameras++;
+        
+        return cameras;
+    }
+};
