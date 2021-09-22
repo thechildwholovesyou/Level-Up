522382cb5c6b86c6eb4f5e352e00df375424f1b9
thechildwholovesyou <ssecutities@gmail.com>
2021-09-22 07:56:57 +0530

    938. Range Sum of BST

--- Binary Trees/level2.cpp
+++ Binary Trees/level2.cpp
@@ -240,3 +240,26 @@ public:
         return isSubtree(root->left , subRoot) || isSubtree(root->right, subRoot);
     }
 };
+
+// 938. Range Sum of BST
+// https://leetcode.com/problems/range-sum-of-bst/
+
+class Solution {
+public:
+        int sum=0;
+    int inorder(TreeNode* root,int L,int R)
+    {
+        if(root){
+        inorder(root->left,L,R);
+        if(root->val>=L && root->val<=R)
+            sum+=root->val;
+        inorder(root->right,L,R);
+        }
+        return sum;
+    }
+    int rangeSumBST(TreeNode* root, int L, int R) {
+        if(!root)return 0;
+        return inorder(root,L,R);
+    }
+    
+};
