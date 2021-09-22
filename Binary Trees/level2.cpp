6cc35a6b9059c435e04200c4575503336263b987
thechildwholovesyou <ssecutities@gmail.com>
2021-09-22 08:20:56 +0530

    110. Balanced Binary Tree

--- Binary Trees/level2.cpp
+++ Binary Trees/level2.cpp
@@ -359,3 +359,39 @@ public:
         return min_diff;
     }
 };
+
+// 110. Balanced Binary Tree
+// https://leetcode.com/problems/balanced-binary-tree/
+
+class Solution {
+public:
+    pair<int,bool> isBalancedHelper(TreeNode* root)
+    {
+       if(root==NULL) {return {-1,true};}
+        
+        pair<int,bool> lpair=isBalancedHelper(root->left);
+        if((lpair.second)==false) return lpair;
+        
+        pair<int,bool> rpair=isBalancedHelper(root->right);
+        if((rpair.second)==false) return rpair;
+        
+        pair<int,bool> ans;
+        
+        if(abs(lpair.first - rpair.first)>1)
+        {
+            // ans.second=false;
+            return ans;
+        }
+        ans.second=true;
+        
+        ans.first = max(lpair.first , rpair.first )+1;
+        
+        return ans;
+        
+        
+    }
+    bool isBalanced(TreeNode* root) {
+        pair<int,bool> ans=isBalancedHelper(root);
+        return ans.second;
+    }
+};
