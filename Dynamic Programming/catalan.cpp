55a9f9e36ad156a5d91e92f0dd5b3348a1ac41a0
thechildwholovesyou <ssecutities@gmail.com>
2021-09-25 12:07:17 +0530

    95. Unique Binary Search Trees II

--- Dynamic Programming/catalan.cpp
+++ Dynamic Programming/catalan.cpp
@@ -47,3 +47,46 @@ public:
     }
 };
 
+// 95. Unique Binary Search Trees II
+// https://leetcode.com/problems/unique-binary-search-trees-ii/
+
+class Solution {
+public:
+    
+
+    
+    vector<TreeNode*> generateTrees_helper(int i,int j) {
+        vector<TreeNode*> v;
+        
+        if(i>j){
+            v.push_back(NULL);
+            return v;
+        }
+        
+        for(int k=i;k<=j;k++)
+        {
+            vector<TreeNode*> left = generateTrees_helper(i,k-1);
+            vector<TreeNode*> right = generateTrees_helper(k+1,j);
+            
+            for(auto l: left)
+            {
+                for(auto r: right)
+                {
+                    TreeNode* root=new TreeNode(k);
+                    root->left=l;
+                    root->right=r;
+                    v.push_back(root);
+                }
+            }
+        }
+        return v;
+    }
+    
+    
+    vector<TreeNode*> generateTrees(int n) {
+        vector<TreeNode* > v;
+        if(n==0) return v;
+        
+        return generateTrees_helper(1,n);
+    }
+};
