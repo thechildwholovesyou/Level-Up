1deb474e6e00c7ecc58b574d072614859e67d27e
thechildwholovesyou <ssecutities@gmail.com>
2021-09-30 19:35:07 +0530

    using arrays

--- Stackk And Queues/construtction.cpp
+++ Stackk And Queues/construtction.cpp
@@ -33,3 +33,65 @@ int main()
     }
     return 0;
 }
+
+// construction using arrays 
+
+#include<bits/stdc++.h>
+using namespace std;
+#define max 100
+
+class Stack{
+    public:
+    int top;
+    int arr[max];
+
+    void push(int data){
+        if(top==max){
+            cout<<"Stack overflow"<<endl;
+            return;
+        }
+        else
+        {
+            top++;
+            arr[top]=data;
+        }
+    }
+    void peek(){
+        if(top==-1){
+            cout<<"Stack underflowed"<<endl;
+            return;
+        }
+        cout<<arr[top];
+    }
+    void pop(){
+        if(top==-1){
+            cout<<"Stack underflowed"<<endl;
+            return;
+        }
+        else{
+            cout<<arr[top]<<endl;
+            top--;
+        }
+    }
+    bool empty(){
+        if(top==0) return true;
+        else
+        return false;
+    }
+    Stack(){
+        top=-1;
+    }
+};
+
+int main()
+{
+    Stack s;
+    for(int i=0;i<6;i++){
+        s.push(i*i);
+    }
+
+    while(!s.empty()){
+        s.pop();
+    }
+    return 0 ;
+}
