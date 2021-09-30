912964ee577cc40a20b68a7a90e2e39fe058c353
thechildwholovesyou <ssecutities@gmail.com>
2021-09-30 22:12:29 +0530

    232. Implement Queue using Stacks

--- Stackk And Queues/leetcode.cpp
+++ Stackk And Queues/leetcode.cpp
@@ -112,3 +112,104 @@ public:
     }
 };
 
+
+
+//
+// https://leetcode.com/problems/implement-queue-using-stacks/
+
+// POP operation costly 
+
+class MyQueue {
+public:
+    stack<int> s1;
+    stack<int> s2;
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        s1.push(x);
+    }
+    
+    int pop() {
+         while(!s1.empty())
+        {
+            s2.push(s1.top());
+             s1.pop();
+        }
+        int x=s2.top();
+        s2.pop();
+        while(!s2.empty())
+        {
+            s1.push(s2.top());
+            s2.pop();
+        }
+        return x;
+    }
+    
+    int peek() {
+         while(!s1.empty())
+        {
+            s2.push(s1.top());
+             s1.pop();
+        }
+        int x=s2.top();
+        
+        while(!s2.empty())
+        {
+            s1.push(s2.top());
+            s2.pop();
+        }
+        return x;
+    }
+    
+    bool empty() {
+        if(s1.size()==0) return true;
+        return false;
+    }
+};
+
+// push operaion costly 
+
+class MyQueue {
+public:
+    /** Initialize your data structure here. */
+    stack<int> s1;
+    stack<int> s2;
+    MyQueue() {
+        
+    }
+    
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        while(!s1.empty()){
+            int x=s1.top();
+            s2.push(x);
+            s1.pop();
+        }
+        s2.push(x);
+        while(!s2.empty()){
+            int x=s2.top();
+            s1.push(x);
+            s2.pop();
+        }
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        int x=s1.top();
+        s1.pop();
+        return x;
+    }
+    
+    /** Get the front element. */
+    int peek() {
+        return s1.top();
+    }
+    
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return (s1.size()==0);
+    }
+};
+
