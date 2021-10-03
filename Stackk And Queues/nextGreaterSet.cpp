1da4d1bdb04dc618f1ad4cbc5e49b1b8c83cf2e4
thechildwholovesyou <ssecutities@gmail.com>
2021-10-03 20:16:28 +0530

    next greater on left

--- Stackk And Queues/nextGreaterSet.cpp
+++ Stackk And Queues/nextGreaterSet.cpp
@@ -14,7 +14,26 @@ vector<int> ngor (vector<int>&arr)
         }
         s.push(i);
     }
+    return ans;
 }
 
-// next smaller on right 
+// next greater on left  
 
+vector<int> ngol (vector<int>&arr)
+{
+    int n=arr.size();
+    vector<int> ans(n, -1);
+    stack<int> s;
+    s.push(-1);
+    for(int i=n-1;i>=0;i++)
+    {
+        while(s.top()!=-1 and arr[st.top()]< st.top())
+        {
+            ans[s.pop()]=i;
+        }
+        s.push(i);
+    }
+    return ans;
+}
+
+//
