87104689d1391cdc848692a6591607eb4ad4214f
thechildwholovesyou <ssecutities@gmail.com>
2021-10-03 20:50:02 +0530

    503. Next Greater Element II

--- Stackk And Queues/nextGreaterSet.cpp
+++ Stackk And Queues/nextGreaterSet.cpp
@@ -36,4 +36,46 @@ vector<int> ngol (vector<int>&arr)
     return ans;
 }
 
-//
+// next smaller on right
+
+vector<int> ngor (vector<int>&arr)
+{
+    int n=arr.size();
+    vector<int> ans(n, -1);
+    stack<int> s;
+    s.push(-1);
+    for(int i=0;i<n;i++)
+    {
+        while(s.top()!=-1 and arr[st.top()]> st.top())
+        {
+            ans[s.pop()]=i;
+        }
+        s.push(i);
+    }
+    return ans;
+}
+
+// ------------------------ LEETCODE --------------------//
+
+//503. Next Greater Element II
+// https://leetcode.com/problems/next-greater-element-ii/
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& arr) {
+    int n=arr.size();
+    vector<int> ans(n, -1);
+    stack<int> s;
+    s.push(-1);
+    for(int i=0;i<n*2;i++)
+    {
+        while(s.top()!=-1 and arr[s.top()]< arr[i%n])
+        {
+            ans[s.top()]=arr[i%n];
+            s.pop();
+        }
+        s.push(i%n);
+    }
+    return ans;
+    }
+};
