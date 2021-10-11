9d21c74038008b5d6d618e77b7dc79c8d25b8a78
thechildwholovesyou <ssecutities@gmail.com>
2021-10-11 21:38:30 +0530

    155. Min Stack

--- Stackk And Queues/9oct.cpp
+++ Stackk And Queues/9oct.cpp
@@ -301,3 +301,47 @@ public:
  * obj->push(val);
  * int param_2 = obj->pop();
  */
+
+
+// 155. Min Stack
+// https://leetcode.com/problems/min-stack/
+
+class MinStack {
+public:
+    vector<pair<int,int>> stk;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(stk.empty())
+        {
+            stk.push_back({val,val});
+        }
+        else
+        {
+            stk.push_back({val,min(stk.back().second, val)});
+        }
+    }
+    
+    void pop() {
+        stk.pop_back();
+    }
+    
+    int top() {
+        return stk.back().first;
+    }
+    
+    int getMin() {
+        return stk.back().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
