ddb3734faa8e143e4d4de2c8710df297a00329e1
thechildwholovesyou <ssecutities@gmail.com>
2021-10-13 20:56:51 +0530

    347. Top K Frequent Elements

---
+++ HashMap and Heaps/leetcode.cpp
@@ -0,0 +1,24 @@
+// 347. Top K Frequent Elements
+// https://leetcode.com/problems/top-k-frequent-elements/
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        priority_queue<pair<int,int>> pq;
+        map<int,int> m;
+        for(auto ele: nums){
+            m[ele]++;
+        }
+        
+        for(auto ele: m){
+            pq.push({ele.second, ele.first});
+        }
+        
+        vector<int> v;
+        while(k--){
+            v.push_back(pq.top().second);
+            pq.pop();
+        }
+        return v;
+    }
+};
