75afdb54cde44c0c9d7a7b4f05dc6c2232a90fdd
thechildwholovesyou <ssecutities@gmail.com>
2021-10-16 20:42:55 +0530

    703. Kth Largest Element in a Stream

--- HashMap and Heaps/leetcode.cpp
+++ HashMap and Heaps/leetcode.cpp
@@ -187,6 +187,10 @@ public:
     }
 };
 
+// optimal sol 
+// 
+
+
 // Kth smallest element
 // https://practice.geeksforgeeks.org/problems/kth-smallest-element5635/1#
 
@@ -250,3 +254,34 @@ public:
         return p.top();
     }
 };
+
+// 703. Kth Largest Element in a Stream
+// https://leetcode.com/problems/kth-largest-element-in-a-stream/
+
+class KthLargest {
+public:
+    priority_queue<int,vector<int>,greater<int>> pq;
+    int K;
+    KthLargest(int k, vector<int>& nums) {
+        K=k;
+        for(auto ele:nums)
+        {
+            pq.push(ele);
+            if(pq.size()>K)
+                pq.pop();
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if(pq.size()>K)
+            pq.pop();
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
