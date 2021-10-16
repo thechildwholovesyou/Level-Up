099076dae1b79c63f326759db64ad3963ee53d9a
thechildwholovesyou <ssecutities@gmail.com>
2021-10-16 19:51:09 +0530

    Kth smallest element

--- HashMap and Heaps/leetcode.cpp
+++ HashMap and Heaps/leetcode.cpp
@@ -185,3 +185,23 @@ public:
         return pq.top();
     }
 };
+
+// Kth smallest element
+// https://practice.geeksforgeeks.org/problems/kth-smallest-element5635/1#
+
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        int size=(r-l)+1;
+        priority_queue<int>pq;// min heap
+        for(int i=0;i<size;i++)
+            pq.push(arr[i]);
+        while(pq.size() > k) pq.pop(); 
+        return pq.top();
+    }
+};
