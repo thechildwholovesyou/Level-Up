4d4467ab24ec213f430648780c0e92b0ef0a31eb
thechildwholovesyou <ssecutities@gmail.com>
2021-10-23 09:42:34 +0530

    update

--- Binary Trees/level2.cpp
+++ Binary Trees/level2.cpp
@@ -539,3 +539,52 @@ public:
         return node;
     }
 };
+
+// java code
+ /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode helper(int[] preorder , int[] inorder, int ps,int pe,int is,int ie)
+    {
+        // base case 
+        if(ps>pe)
+            return null;
+        
+        // make root using pre first element 
+        TreeNode node= new TreeNode(preorder[ps]);
+        // search the node in inorder array 
+        int k=-1;
+        for(int i=is;i<=ie;i++)
+        {
+            if(inorder[i]==node.val)
+            {
+                k=i;
+                break;
+            }
+        }
+        // count left subtree nodes wtr root node
+        int cls=k-is;
+        
+        node.left=helper(preorder, inorder, ps+1,ps+cls,is,k-1);
+        node.right=helper(preorder, inorder, ps+cls+1,pe,k+1,ie);
+        return node;
+    }
+    public TreeNode buildTree(int[] preorder , int[] inorder) {
+        int n=preorder.length;
+        TreeNode root=helper(preorder, inorder, 0,n-1,0,n-1);
+        return root;
+    }
+}
