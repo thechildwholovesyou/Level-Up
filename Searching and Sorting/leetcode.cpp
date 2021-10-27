456cfa1e56433b131239f7770692f24e15298f30
thechildwholovesyou <ssecutities@gmail.com>
2021-10-27 21:21:02 +0530

    658. Find K Closest Elements-1

--- Searching and Sorting/leetcode.cpp
+++ Searching and Sorting/leetcode.cpp
@@ -141,3 +141,28 @@ public:
         return l; // return h will also work as l and h will be in the same pos
     }
 };
+
+// 658. Find K Closest Elements
+// https://leetcode.com/problems/find-k-closest-elements/
+// using Priority queues
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pair<int, int>> pq;
+        for(auto ele: arr)
+        {
+            pq.push({abs(x-ele), ele});
+            if(pq.size()>k)
+                pq.pop();
+        }
+        vector<int> ans;
+        while(!pq.empty())
+        {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
