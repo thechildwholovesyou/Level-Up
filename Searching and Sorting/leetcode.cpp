3ab781e546b4968a9e6706252a1c411b35dd1922
thechildwholovesyou <ssecutities@gmail.com>
2021-10-29 20:01:20 +0530

    1. Two Sum-1

--- Searching and Sorting/leetcode.cpp
+++ Searching and Sorting/leetcode.cpp
@@ -208,3 +208,24 @@ public:
         return {arr.begin()+low, arr.begin()+high+1};
     }
 };
+
+// 1. Two Sum
+// https://leetcode.com/problems/two-sum/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        unordered_map<int, int> mp;
+        for(int i=0; i<nums.size(); i++){
+            if(mp.find(target-nums[i]) != mp.end()){
+                ans.push_back(mp[target-nums[i]]);
+                ans.push_back(i);
+                return ans;
+            }
+            else
+                mp[nums[i]]=i;
+        }
+        return ans;
+    }
+};
