dc641a1a6536cecfb6fe6391eba3fa980b8957ee
thechildwholovesyou <ssecutities@gmail.com>
2021-11-01 09:14:15 +0530

    617. Merge Two Binary Trees

--- Binary Trees/leetcode.cpp
+++ Binary Trees/leetcode.cpp
@@ -544,3 +544,4 @@ public:
         return v;
     }
 };
+
--- Binary Trees/level2.cpp
+++ Binary Trees/level2.cpp
@@ -1020,3 +1020,20 @@ public:
         
     }
 };
+
+// 617. Merge Two Binary Trees
+// https://leetcode.com/problems/merge-two-binary-trees/
+
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        if(!root1) return root2;
+        if(!root2) return root1;
+        
+        root1->val+=root2->val;
+        root1->left=mergeTrees(root1->left,root2->left);
+        root1->right=mergeTrees(root1->right,root2->right);
+        
+        return root1;
+    }
+};
