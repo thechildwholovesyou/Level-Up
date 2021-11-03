c6e6b5e688db75f83edcacb95139b63c430732b3
thechildwholovesyou <ssecutities@gmail.com>
2021-11-03 09:43:03 +0530

    update

--- Binary Trees/level2.cpp
+++ Binary Trees/level2.cpp
@@ -1329,3 +1329,41 @@ public:
         
     }
 };
+
+// optimal approach 
+
+class Solution {
+       public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null) return null;
+        
+        if(root.val < key){
+            root.right = deleteNode(root.right, key);
+        }else if(root.val > key){
+            root.left = deleteNode(root.left, key);
+        }else{
+            if(root.left != null && root.right != null){
+                int v = findmax(root.left, Integer.MIN_VALUE);
+                root.val = v;
+                root.left = deleteNode(root.left, v);
+                return root;
+            }else if(root.left != null){
+                return root.left;
+            }else if(root.right != null){
+                return root.right;
+            }else{
+                return null;
+            }
+            
+        }
+        return root;
+    }
+    
+    
+    private static int findmax(TreeNode root, int max){
+       while(root !=null){
+           max = Math.max(max, root.val);
+           root = root.right;
+       }
+        return max;
+    }
+}
