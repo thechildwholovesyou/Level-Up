7b5f9d2c9ec76167cd86492d40e2c7eab47ce078
thechildwholovesyou <ssecutities@gmail.com>
2021-11-03 08:44:34 +0530

    update

--- Binary Trees/level2.cpp
+++ Binary Trees/level2.cpp
@@ -1274,3 +1274,20 @@ public:
         return sum;
     }
 };
+
+// another DFS approach 
+class Solution {
+    private int dfs(TreeNode node, int path) {
+    if (node == null) {
+        return 0;
+    }
+    path = path*10 + node.val;
+    if (node.left == null && node.right == null) {
+        return path;
+    }
+    return dfs(node.left, path) + dfs(node.right, path);
+}
+    public int sumNumbers(TreeNode root) {
+        return dfs(root,0);
+    }
+}
