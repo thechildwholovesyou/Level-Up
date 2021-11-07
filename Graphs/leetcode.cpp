167a00986022c49c5e1f959a53d682b32158e851
thechildwholovesyou <ssecutities@gmail.com>
2021-11-07 22:28:06 +0530

    1319. Number of Operations to Make Network Connected

--- Graphs/leetcode.cpp
+++ Graphs/leetcode.cpp
@@ -64,3 +64,37 @@ public:
         return bigAns;
     }
 };
+
+// 1319. Number of Operations to Make Network Connected
+// https://leetcode.com/problems/number-of-operations-to-make-network-connected/
+
+class Solution {
+public:
+     void dfs(vector<vector<int>> &adj, vector<bool> &visited, int src)
+    {
+        visited[src] = true;
+        for(int i : adj[src])
+            if(!visited[i])
+                dfs(adj, visited, i);
+    }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        
+        if(connections.size() < n - 1)
+            return -1;
+        vector<vector<int>> adj(n);
+        for(auto v : connections)
+        {
+            adj[v[0]].push_back(v[1]);
+            adj[v[1]].push_back(v[0]);
+        }
+        vector<bool> visited(n, false);
+        int components = 0;
+        for(int i=0; i<n; i++)
+            if(!visited[i])
+            {
+                dfs(adj, visited, i);
+                components++;
+            }
+        return components - 1;
+    }
+};
