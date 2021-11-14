8d7d56ca8e8930da56bddbe9a76d550e4eca9bda
thechildwholovesyou <ssecutities@gmail.com>
2021-11-14 13:23:44 +0530

    First negative integer in every window of size k

--- Sliding Window/aditya_verma.cpp
+++ Sliding Window/aditya_verma.cpp
@@ -25,3 +25,39 @@ public:
         return maxi;
     }
 };
+
+// First negative integer in every window of size k 
+// https://practice.geeksforgeeks.org/problems/first-negative-integer-in-every-window-of-size-k3345/1
+// Time limit exceeded
+
+vector<long long> printFirstNegativeInteger(long long int A[],long long int N, long long int k) {
+    
+    long long int i=0;
+    long long int j=0;
+    
+    vector<long long >v;
+    while(j<N)
+    {
+        if(j-i+1<k)
+        {
+            j++;
+        }
+        else if(j-i+1==k)
+        {
+            for(int p=i;p<=j;p++)
+            {
+                if(A[p]<0)
+                {
+                    v.push_back(A[p]);
+                    break;
+                }
+                if(p==j and A[p]>0)
+                    v.push_back(0);
+            }
+            i++;
+            j++;
+        }
+    }
+    return v;
+    
+ }
