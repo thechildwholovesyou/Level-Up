0e7580f65675d5b53811a42378a6eeba9e0d86ba
thechildwholovesyou <ssecutities@gmail.com>
2021-11-14 15:49:48 +0530

    optimised

--- Sliding Window/aditya_verma.cpp
+++ Sliding Window/aditya_verma.cpp
@@ -61,3 +61,35 @@ vector<long long> printFirstNegativeInteger(long long int A[],long long int N, l
     return v;
     
  }
+
+ // optimised
+
+ vector<long long> printFirstNegativeInteger(long long int arr[],
+                                            long long int N, long long int k) {
+
+    list<int>l;
+   vector<long long>v;
+   int i=0,j=0;
+   while(j<N)
+   {    
+       if(arr[j]<0)
+       l.push_back(arr[j]);
+       if(j-i+1<k)
+       j++;
+       else if(j-i+1==k)
+       {
+          
+          
+          if(l.size()!=0)
+          v.push_back(l.front());
+          else
+          v.push_back(0);
+          if(l.front()==arr[i] && l.size()>0)
+          l.pop_front();
+         i++;j++;
+           
+       }
+   }
+       return v;
+       
+   }
