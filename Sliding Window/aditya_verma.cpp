0f9db485b3db2aa2b4c861d5e3cef7bc6be00d6f
thechildwholovesyou <ssecutities@gmail.com>
2021-11-14 16:34:19 +0530

    Count Occurences of Anagrams

--- Sliding Window/aditya_verma.cpp
+++ Sliding Window/aditya_verma.cpp
@@ -150,3 +150,42 @@ public:
 	}
 
 };
+
+// Count Occurences of Anagrams 
+// https://practice.geeksforgeeks.org/problems/count-occurences-of-anagrams5839/1
+
+int solve(string s, string ana){
+
+    unordered_map<char, int> m;
+    for(auto it : ana) m[it]++;
+
+    int k=ana.length();
+    int count=m.size();
+    
+    int i=0, j=0;
+    int ans=0;
+    
+    while(j<s.length()){
+    
+        if(m.find(s[j])!=m.end()){
+            m[s[j]]--;
+            if(m[s[j]]==0) count--;
+        }
+        
+        if((j-i+1)<k) j++;
+        
+        else if((j-i+1)==k){
+
+            if(count==0) ans++;
+
+            if(m.find(s[i])!=m.end()){
+                m[s[i]]++;
+                if(m[s[i]]==1) count++;
+            }
+
+            i++;
+            j++;
+        }
+    }
+    return ans;
+}
