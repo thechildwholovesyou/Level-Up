e87683740236a226637900d4de824a38488f337f
thechildwholovesyou <ssecutities@gmail.com>
2021-11-14 16:14:15 +0530

    brute-1

--- Sliding Window/aditya_verma.cpp
+++ Sliding Window/aditya_verma.cpp
@@ -93,3 +93,60 @@ vector<long long> printFirstNegativeInteger(long long int A[],long long int N, l
        return v;
        
    }
+
+
+// Count Occurences of Anagrams 
+// https://practice.geeksforgeeks.org/problems/count-occurences-of-anagrams5839/1
+// brute 
+
+class Solution{
+public:
+    
+    bool helper(unordered_map<char, int >m1, unordered_map<char, int >m2)
+    {
+        for(int i=0;i<m1.size();i++)
+        {
+            if(m1[i]!=m2[i]) return false;
+        }
+        return true;
+    }
+
+	int search(string pat, string txt) {
+	    // code here
+	    int k=pat.size();
+	    sort(pat.begin(),pat.end());
+	    unordered_map<char,int> m;
+	    for(auto ele:pat)
+	        m[ele]++;
+	       
+	   int i=0;
+	   int j=0;
+	   int cnt=0;
+	   while(j<txt.size())
+	   {
+	       if(j-i+1<k)
+	        {
+	            j++;
+	        }
+	       else if(j-i+1==k)
+	       {
+	           string temp="";
+	           for(int p=i;p<=j;p++)
+	                temp+=txt[p];
+	               
+	           sort(temp.begin(),temp.end());
+	           unordered_map<char,int> t;
+	           for(auto ele:temp)
+	           {
+	               t[ele]++;
+	           }
+	           
+	           if(helper(m,t)) cnt++;
+	           i++;
+	           j++;
+	       }
+	   }
+	   return cnt;
+	}
+
+};
