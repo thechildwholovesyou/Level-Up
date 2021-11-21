0c146152e620a936c12d4816acf03e8ad909e684
thechildwholovesyou <ssecutities@gmail.com>
2021-11-21 19:46:42 +0530

    Minimum Platforms

--- Greedy Algorithms/striver.cpp
+++ Greedy Algorithms/striver.cpp
@@ -47,3 +47,37 @@ class Solution
         
     }
 };
+
+// Minimum Platforms
+// https://practice.geeksforgeeks.org/problems/minimum-platforms-1587115620/1
+
+class Solution{
+    public:
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    int findPlatform(int arr[], int dep[], int n)
+    {
+    	// Your code here
+    	sort(arr,arr+n);
+    	sort(dep,dep+n);
+    	int i=1,j=0;
+    	
+    	int platform=1,res=1;
+    	
+    	while(i<n and j<n)
+    	{
+    	    if(arr[i]<=dep[j])
+    	    {
+    	        platform++;
+    	        i++;
+    	    }
+    	    else if(arr[i]>dep[j])
+    	    {
+    	        platform--;
+    	        j++;
+    	    }
+    	    res=max(res,platform);
+    	}
+    	return res;
+    }
+};
