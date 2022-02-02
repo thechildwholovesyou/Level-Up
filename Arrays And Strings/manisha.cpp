d7acae3746a73129d1268a7b477261f52de22b91
thechildwholovesyou <ssecutities@gmail.com>
2022-02-02 11:05:09 +0530

    update

--- Arrays And Strings/manisha.cpp
+++ Arrays And Strings/manisha.cpp
@@ -429,3 +429,108 @@ public:
         return to_string(a*c-b*d)+'+'+to_string(a*d+b*c)+'i';
     }
 };
+
+// 41. First Missing Positive
+// https://leetcode.com/problems/first-missing-positive/
+// O(N) O(1)
+
+class Solution {
+public:
+    int seggregate(vector<int>&nums)
+    {
+        int i=0;
+        int j=0;
+        
+        while(j<nums.size())
+        {
+            if(nums[j]<=0) j++;
+            else
+            {
+                swap(nums[i], nums[j]);
+                i++;
+                j++;
+            }
+        }
+        return i-1;
+    }
+    int firstMissingPositive(vector<int>& nums) {
+        int pi=seggregate(nums); // pi=> positive index
+        
+        for(int i=0;i<=pi;i++)
+        {
+            int idx=abs(nums[i])-1;
+            if(idx<=pi and nums[idx]>0)
+                nums[idx]=-nums[idx];
+        }
+        
+        for(int i=0;i<=pi;i++)
+        {
+            if(nums[i]>0)
+                return i+1;
+        }
+        return pi+2;
+    }
+};
+
+// 628. Maximum Product of Three Numbers
+// https://leetcode.com/problems/maximum-product-of-three-numbers/
+// nlogn 
+
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        // nlogn 
+        int n=nums.size();
+        sort(nums.rbegin(),nums.rend());
+        int res1=1;
+        int maxi=1;
+        for(int i=0;i<=2;i++)
+            res1*=nums[i];
+        int res2=nums[n-1]*nums[n-2]*nums[0];
+        
+        return max(res1, res2);
+    }
+};
+
+// O(N) O(1)
+
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        // simply find out te largest 3 elements and 2 smallest elements
+        int max1=INT_MIN;
+        int max2=INT_MIN;
+        int max3=INT_MIN;
+        int min1=INT_MAX;
+        int min2=INT_MAX;
+        
+        for(auto ele: nums)
+        {
+            if(ele>max1)
+            {
+                max3=max2;
+                max2=max1;
+                max1=ele;
+            }
+            else if(ele>max2)
+            {
+                max3=max2;
+                max2=ele;
+            }
+            else if(ele>max3)
+            {
+                max3=ele;
+            }
+            if(ele<min1)
+            {
+                min2=min1;
+                min1=ele;
+            }
+            else if(ele<min2)
+            {
+                min2=ele;
+            }
+        }
+        return max(max1*max2*max3, max1*min1*min2);
+    }
+};
