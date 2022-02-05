31e2a5f3045fb4a604ced03a8d0e97cf8ead0365
thechildwholovesyou <ssecutities@gmail.com>
2022-02-05 11:44:22 +0530

    update

--- Arrays And Strings/manisha.cpp
+++ Arrays And Strings/manisha.cpp
@@ -728,3 +728,26 @@ public:
         return bigAns;
     }
 };
+
+// 48. Rotate Image
+// https://leetcode.com/problems/rotate-image/
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        // transpose the matrix 
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=i+1;j<matrix[0].size();j++)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        
+        // reverse each row
+        for(int i=0;i<matrix.size();i++)
+        {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
